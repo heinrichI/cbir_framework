f072f30ca3d7dc2ae22edcd87a920adf895b13b9
dimathe47 <dimathe47@yandex.ru>
2017-05-15 23:22:06 +0300

    add tests rebase(delete binary files)

--- tests/test_inverted_multi_index_searcher.py
+++ tests/test_inverted_multi_index_searcher.py
@@ -14,7 +14,7 @@ class InvertedMultiIndexSearcherTest(unittest.TestCase):
             [10, 2, 60, 5],
             [10, 5, 60, 10]
         ], dtype=np.float32)
-        ids_ndarray = np.arange(1, 5)
+        ids_ndarray = np.array([10, 20, 30, 40, 50], dtype=np.int32)
 
         query_vectors = np.array([
             [0, 2, 4, 5],
@@ -24,17 +24,17 @@ class InvertedMultiIndexSearcherTest(unittest.TestCase):
         pq = pq_quantizer.PQQuantizer(n_clusters=2, n_quantizers=2)
         pq.fit(base_vectors)
         cluster_centers = pq.get_cluster_centers()
-
         searcher_ = imis.InvertedMultiIndexSearcher(x=base_vectors, x_ids=ids_ndarray,
                                                     cluster_centers=cluster_centers)
 
         nearest_ids_ndarray = searcher_.find_nearest_ids(query_vectors, n_nearest=4)
-        truth_indices = np.array([
-            [0, 1, 2, 3],
-            [2, 3, 1, 0],
-        ])
-        truth_ids = truth_indices + 1
-        self.assertTrue((truth_ids.shape == nearest_ids_ndarray.shape))
+        print(nearest_ids_ndarray)
+
+        self.assertEqual(set(nearest_ids_ndarray[0,0:2]), {10, 20})
+        self.assertEqual(set(nearest_ids_ndarray[0,2:5]), {30, 40})
+
+        self.assertEqual(set(nearest_ids_ndarray[1, 0:2]), {30, 40})
+        self.assertEqual(set(nearest_ids_ndarray[1, 2:5]), {10, 20})
 
 
 if __name__ == '__main__':
